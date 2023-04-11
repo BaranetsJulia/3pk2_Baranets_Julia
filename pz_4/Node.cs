d15e58d64f3e84966720dc484ae30575432df845
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2023-04-11 19:32:07 +0500

    init

---
+++ pz_4/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_4
+{
+    internal class Node
+    {
+        public class DTreeNode // Класс «Узел дихотомического дерева»
+        {
+            private char info; // информационное поле
+            private int key; // поле ключа
+            private DTreeNode left; // ссылка на левое поддерево
+            private DTreeNode right; // ссылка на правое поддерево
+            public char Info { get; set; } // свойства
+            public int Key { get; set; }
+            public DTreeNode Left { get; set; }
+            public DTreeNode Right { get; set; }
+            public DTreeNode() { } // конструкторы
+            public DTreeNode(char info, int key)
+            {
+                Info = info; Key = key;
+            }
+
+
+            public DTreeNode(char info, int key, DTreeNode left, DTreeNode right)
+            {
+                Info = info; Key = key; Left = left; Right = right;
+            }
+        }
+    }
+}
